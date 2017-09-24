1c1b385cc200e6ba8b5c85829fc79f17a032d30e
Tom Adams <tomjadams@gmail.com>
2017-09-24 17:47:30 +1000

    Fixed formatting

--- src/test/scala/com/redbubble/hawk/HawkAuthenticateRequestFilterSpec.scala
+++ src/test/scala/com/redbubble/hawk/HawkAuthenticateRequestFilterSpec.scala
@@ -28,17 +28,17 @@ final class HawkAuthenticateRequestFilterSpec extends Specification with SpecHel
   "HawkAuthenticateRequestFilter" >> {
     val hawkAuthenticatedService = HawkAuthFilter andThen TestService
 
-        "with an invalid Hawk authorisation header" >> {
-          val timeStamp = Time.nowUtc.millis
-          val nonce = Nonce.generateNonce
-          val invalidHawkHeader = s"""Hawk id="dh37fgj492je", ts="$timeStamp", nonce="$nonce", hash="Yi9LfIIFRtBEPt74PVmbTF/xVAwPn7ub15ePICfgnuY=", ext="some-app-ext-data", mac="aSe1DERmZuRl3pI36/9BdZmnErTw3sNzOOAUlfeKjVw=""""
-          val req = requestWithAuthHeader(URI.create("/uri/does/not/matter"), invalidHawkHeader)
+    "with an invalid Hawk authorisation header" >> {
+      val timeStamp = Time.nowUtc.millis
+      val nonce = Nonce.generateNonce
+      val invalidHawkHeader = s"""Hawk id="dh37fgj492je", ts="$timeStamp", nonce="$nonce", hash="Yi9LfIIFRtBEPt74PVmbTF/xVAwPn7ub15ePICfgnuY=", ext="some-app-ext-data", mac="aSe1DERmZuRl3pI36/9BdZmnErTw3sNzOOAUlfeKjVw=""""
+      val req = requestWithAuthHeader(URI.create("/uri/does/not/matter"), invalidHawkHeader)
 
-          "does not execute the service" >> {
-            val result = Await.result(hawkAuthenticatedService(req))
-            result.status should be(Unauthorized)
-          }
-        }
+      "does not execute the service" >> {
+        val result = Await.result(hawkAuthenticatedService(req))
+        result.status should be(Unauthorized)
+      }
+    }
 
     "with a valid Hawk authorisation header" >> {
       val uri = URI.create("https://example.com/foo/bar")
